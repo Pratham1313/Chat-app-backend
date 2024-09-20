49347570ef8e6649021c61aff9272fe1d058a3f2
Pratham1313 <92097340+Pratham1313@users.noreply.github.com>
2024-09-20 08:40:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,6 +11,15 @@ import userroutes from "./routes/user.js";
 import { app, server } from "./socket/socket.js";
 
 dotenv.config();
+app.use((req, res, next) => {
+    res.set({
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "*",
+        "Access-Control-Allow-Headers": "'Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token'",
+    });
+
+    next();
+});
 
 app.use(
   cors({
